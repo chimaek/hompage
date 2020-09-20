ee4f530a3e0619cd0576c031f8a23898cb4a7a41
chimaek <pipiru100@gmail.com>
2020-09-20 13:23:46 +0900

    Updates

---
+++ precache-manifest.931ae6c5f3bf35d8f0a28460b06d9e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bffcf821197245283d9c0eb498a36eb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d00fd4fc98e2ed06e0f",
+    "url": "/portfolio/static/css/main.71c9a8a9.chunk.css"
+  },
+  {
+    "revision": "cf9f6aab99d8744578de",
+    "url": "/portfolio/static/js/2.f76effb3.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/portfolio/static/js/2.f76effb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d00fd4fc98e2ed06e0f",
+    "url": "/portfolio/static/js/main.9f0190ea.chunk.js"
+  },
+  {
+    "revision": "e345a16de91e7c06ddb7",
+    "url": "/portfolio/static/js/runtime-main.1879ff2c.js"
+  }
+]);
