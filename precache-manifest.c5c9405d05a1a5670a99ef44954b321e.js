5961b33a757ef5a0c81fad10589a2cd183d389d9
chimaek <pipiru100@gmail.com>
2020-09-20 14:25:02 +0900

    Updates

--- precache-manifest.c5c9405d05a1a5670a99ef44954b321e.js
+++ precache-manifest.c5c9405d05a1a5670a99ef44954b321e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "946bf8f70e2c0d957fe352a8e8bc03a9",
+    "revision": "f0ab29966e65557c890bc27211b18f84",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "97aa557c8bc7cfb5c1ea",
+    "revision": "6ae313a61559e442e421",
     "url": "/portfolio/static/css/main.71c9a8a9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.131882d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97aa557c8bc7cfb5c1ea",
-    "url": "/portfolio/static/js/main.9470621b.chunk.js"
+    "revision": "6ae313a61559e442e421",
+    "url": "/portfolio/static/js/main.c5677d48.chunk.js"
   },
   {
     "revision": "e345a16de91e7c06ddb7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fa536890ccd0180889b9d0231db09400.js"
+  "/portfolio/precache-manifest.c5c9405d05a1a5670a99ef44954b321e.js"
 );
 
 self.addEventListener('message', (event) => {
