a81e315652ad86b30e5ede20dd4653469836af86
chimaek <pipiru100@gmail.com>
2021-04-27 17:58:02 +0900

    기존 포폴 지우기

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Profile from './Profile';
-import Career from './Career';
-import Skill from './Skill';
-import Project from './Project';
-import Presentation from './Presentation';
-import Education from './Education';
-import App from './App';
-
-export { App, Profile, Career, Skill, Project, Presentation, Education };
